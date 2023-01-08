365464ebe40a774d541fe810c6a09bfa66ce2be3
Chartyx04652 <chardie09213996663@gmail.com>
2023-01-08 13:11:19 +0800

    Added ScanQrCode winform

---
+++ QrSystem1/QrSystem1/QrSystem1/ScanQrCode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QrSystem1
+{
+    public partial class ScanQrCode : Form
+    {
+        public ScanQrCode()
+        {
+            InitializeComponent();
+        }
+    }
+}
